b76a8b6cf06732b42930fb941374f3b8c9ebaad6
Nethan <nethanquinnjael@gmail.com>
2023-05-06 10:38:54 +0800

    Initial Release

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import utils
 class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.timezones = []
+        self.timezones = {"UTC":utils.utc_converter, "JST":utils.jst_converter, "ET":utils.et_converter, "CT":utils.ct_converter, "MT":utils.mt_converter, "PT":utils.pt_converter}
         self.app_name = "Time Zone Converter"
 
         self.SCR_WIDTH= self.winfo_screenwidth()
@@ -16,7 +16,8 @@ class MainWindow(tk.Tk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=2)
         self.grid_rowconfigure(0, weight=1)
-        self.reg = self.register(self.filter)
+        self.hr_reg = self.register(self.hour_filter)
+        self.min_reg = self.register(self.minute_filter)
 
         self.gen_frame = ttk.Frame(self, relief="groove")
         self.gen_frame.grid(row=1, column=0, sticky="nsew", padx=30, pady=40)
@@ -33,7 +34,9 @@ class MainWindow(tk.Tk):
         self.input_hr = tk.StringVar()
         self.hour = tk.Entry(self.gen_frame, textvariable=self.input_hr, width=5, justify="center")
         self.hour.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-        self.hour.config(validate="key", validatecommand=(self.reg, '%P'))
+        self.hour.config(validate="key", validatecommand=(self.hr_reg, '%P'))
+        self.hour.bind("<FocusOut>", lambda e: self.on_focusout(e) or self.display(e))
+        self.hour.bind("<KeyRelease>", self.display)
 
         self.title_label = tk.Label(self.gen_frame, text=":")
         self.title_label.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
@@ -41,9 +44,13 @@ class MainWindow(tk.Tk):
         self.input_min = tk.StringVar()
         self.minute = tk.Entry(self.gen_frame, textvariable=self.input_min, width=5, justify="center")
         self.minute.grid(row=0, column=2, sticky="ew", padx=5, pady=5)
-        self.minute.config(validate="key", validatecommand=(self.reg, '%P'))
+        self.minute.config(validate="key", validatecommand=(self.min_reg, '%P'))
+        self.minute.bind("<FocusOut>", lambda e: self.on_focusout(e) or self.display(e))
+        self.minute.bind("<KeyRelease>", self.display)
 
-        self.timezone1 = ttk.Combobox(self.gen_frame, values=self.timezones, state="readonly")
+        self.timezone1_var = tk.StringVar()
+        self.timezone1_var.set("-")
+        self.timezone1 = ttk.Combobox(self.gen_frame, textvariable=self.timezone1_var, state="readonly")
         self.timezone1.grid(row=0, column=3, sticky="ew", padx=5, pady=5)
 
         self.conversion_frame = ttk.Frame(self.gen_frame)
@@ -54,7 +61,9 @@ class MainWindow(tk.Tk):
         for i in range(self.conversion_frame_col_count):
             self.conversion_frame.grid_columnconfigure(i, weight=1)
 
-        self.timezone2 = ttk.Combobox(self.conversion_frame, values=self.timezones, state="readonly")
+        self.timezone2_var = tk.StringVar()
+        self.timezone2_var.set("-")
+        self.timezone2 = ttk.Combobox(self.conversion_frame, textvariable=self.timezone2_var, state="readonly")
         self.timezone2.grid(row=0, column=2, columnspan=3, sticky="ew", padx=5, pady=5)
 
         self.result_frame = ttk.Frame(self.conversion_frame, relief="groove")
@@ -62,18 +71,101 @@ class MainWindow(tk.Tk):
 
         self.result_frame.grid_columnconfigure(0, weight=1)
 
-        self.result = tk.Label(self.result_frame, text="Hello")
+        self.result_text = tk.StringVar()
+        self.result_text.set("00 : 00")
+        self.result = tk.Label(self.result_frame, textvariable=self.result_text)
         self.result.grid(sticky="nsew", padx=10, pady=10)
 
-    def filter(self, key):
-        if key.isdigit() and len(key) <= 2:
-            return True
+        self.timezone1.config(postcommand=lambda :self.update_options(self.timezone1, self.timezone2))
+        self.timezone2.config(postcommand=lambda :self.update_options(self.timezone2, self.timezone1))
+        self.timezone1.bind("<FocusOut>", self.display)
+        self.timezone2.bind("<FocusOut>", self.display)
+        self.timezone1.bind("<<ComboboxSelected>>", self.display)
+        self.timezone2.bind("<<ComboboxSelected>>", self.display)
 
-        elif key is "":
-            return True
+    def update_options(self, w1, w2):
+        w1_picked = w1.get()
+        w2_picked = w2.get()
 
+        timezones = self.timezones.keys()
+        options = list(timezones)[:]
+
+        if w1_picked in options:
+            options.remove(w1_picked)
+
+        if w2_picked in options:
+            options.remove(w2_picked)
+
+        options.insert(0, "-")
+
+        if w1_picked == "-":
+            options.remove("-")
+
+        w1['values'] = options
+
+    def on_focusout(self, event):
+        widget = event.widget
+        widget_text = widget.get()
+        if len(widget_text) == 1:
+            widget.delete(0, tk.END)
+            widget.insert(0, f"{int(widget_text):02d}")
+
+    @utils.time_filter
+    def hour_filter(self, key):
+        if int(key) <= 23:
+            return True
         else:
             return False
+
+    @utils.time_filter
+    def minute_filter(self, key):
+        if int(key) <= 59:
+            return True
+        else:
+            return False
+    
+    def update_result(self, val1=None, val2=None, day=None):
+
+        if val1 == "":
+            val1 = "00"
+
+        if val2 == "":
+            val2 = "00"
+
+        if val1 == None or val2 == None:
+            result = "Error"
+        else:
+            result = f"{val1} : {val2} Day {day:+}"
+
+        self.result_text.set(result)
+        
+
+    def display(self, event):
+        value1 = self.hour.get()
+        value2 = self.minute.get()
+
+        time1 = self.timezone1_var.get()
+        time2 = self.timezone2_var.get()
+
+        try:
+            timezone1 = self.timezones[time1]
+            hour, minute = timezone1(value1, value2, reverse=True)
+
+            timezone2 = self.timezones[time2]
+            value1, value2, day = timezone2(hour, minute)
+        except KeyError as ke:
+            print(ke)
+        
+        except ValueError as ve:
+            print(ve)
+
+        print(time1, time2)
+        print(value1, value2)
+
+        if (self.timezone1.get() != "-") and (self.timezone2.get() != "-"):
+            self.update_result(value1, value2, day=day)
+        else:
+            self.update_result()
 
 if __name__ == "__main__":
     app = MainWindow()
