4e746d75714c7a1b11df39e7cccbc00ef1765a7a
Nethan <nethanquinnjael@gmail.com>
2023-05-26 19:45:38 +0800

    Add Time Formats

--- src/app.py
+++ src/app.py
@@ -4,13 +4,16 @@ import utils
 
 class MainWindow(tk.Tk):
     def __init__(self):
-        super().__init__()
-        self.timezones = {"UTC":utils.utc_converter, "JST":utils.jst_converter, "ET":utils.et_converter, "CT":utils.ct_converter, "MT":utils.mt_converter, "PT":utils.pt_converter}
+        super().__init__() 
+        self.iconbitmap(utils.ICON_IMG)
+        self.btn_img = tk.PhotoImage(file=utils.SWITCH_IMG)
+        self.btn_img = self.btn_img.subsample(3,3)
+        self.timezones = utils.TIMEZONE
         self.app_name = "Time Zone Converter"
 
         self.SCR_WIDTH= self.winfo_screenwidth()
         self.SCR_HEIGHT= self.winfo_screenheight()
-        self.geometry(f"350x250")
+        self.geometry(f"410x270")
         self.title(self.app_name)
         self.resizable(False, False)
         self.grid_columnconfigure(0, weight=1)
@@ -18,13 +21,22 @@ class MainWindow(tk.Tk):
         self.grid_rowconfigure(0, weight=1)
         self.hr_reg = self.register(self.hour_filter)
         self.min_reg = self.register(self.minute_filter)
+        
+        self.menu = tk.Menu(self)
+        self.settings = tk.Menu(self.menu, tearoff=0)
+        self.time_format = tk.StringVar()
+        self.settings.add_radiobutton(label="24-hour Format", variable=self.time_format, value=24, command=self.change_time_format)
+        self.settings.add_radiobutton(label="12-hour Format", variable=self.time_format, value=12, command=self.change_time_format)
+        self.time_format.set(utils.time_format)
+
+        self.settings.add_separator()
+        self.settings.add_command(label="About")
+        self.menu.add_cascade(label="Settings", menu=self.settings)
 
         self.gen_frame = ttk.Frame(self, relief="groove")
         self.gen_frame.grid(row=1, column=0, sticky="nsew", padx=30, pady=40)
-        self.gen_frame.grid_columnconfigure(0, weight=1)
-        self.gen_frame.grid_columnconfigure(1, weight=1)
-        self.gen_frame.grid_columnconfigure(2, weight=1)
-        self.gen_frame.grid_columnconfigure(3, weight=1)
+        for i in range(6):
+            self.gen_frame.grid_columnconfigure(i, weight=1)
         self.gen_frame.grid_rowconfigure(1, weight=1)
         self.gen_frame.grid_rowconfigure(0, weight=1)
 
@@ -35,8 +47,7 @@ class MainWindow(tk.Tk):
         self.hour = tk.Entry(self.gen_frame, textvariable=self.input_hr, width=5, justify="center")
         self.hour.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
         self.hour.config(validate="key", validatecommand=(self.hr_reg, '%P'))
-        self.hour.bind("<FocusOut>", lambda e: self.on_focusout(e) or self.display(e))
-        self.hour.bind("<KeyRelease>", self.display)
+        self.input_hr.set("00")
 
         self.title_label = tk.Label(self.gen_frame, text=":")
         self.title_label.grid(row=0, column=1, sticky="ew", padx=5, pady=5)
@@ -44,19 +55,21 @@ class MainWindow(tk.Tk):
         self.input_min = tk.StringVar()
         self.minute = tk.Entry(self.gen_frame, textvariable=self.input_min, width=5, justify="center")
         self.minute.grid(row=0, column=2, sticky="ew", padx=5, pady=5)
-        self.minute.config(validate="key", validatecommand=(self.min_reg, '%P'))
-        self.minute.bind("<FocusOut>", lambda e: self.on_focusout(e) or self.display(e))
-        self.minute.bind("<KeyRelease>", self.display)
+        self.input_min.set("00")
+
+        self.am_btn = tk.Button(self.gen_frame, text="AM", width=2, command=lambda e=1:self.set_day_type(e))
+        self.am_btn.grid(row=0, column=3, sticky="ew", padx=3, pady=3)
+        self.pm_btn = tk.Button(self.gen_frame, text="PM", width=2, command=lambda e=0:self.set_day_type(e))
+        self.pm_btn.grid(row=0, column=4, sticky="ew", padx=3, pady=3)
 
         self.timezone1_var = tk.StringVar()
         self.timezone1_var.set("-")
         self.timezone1 = ttk.Combobox(self.gen_frame, textvariable=self.timezone1_var, state="readonly")
-        self.timezone1.grid(row=0, column=3, sticky="ew", padx=5, pady=5)
+        self.timezone1.grid(row=0, column=5, sticky="ew", padx=5, pady=5)
 
         self.conversion_frame = ttk.Frame(self.gen_frame)
-        self.conversion_frame.grid(row=2, column=0, columnspan=4, sticky="nsew", padx=5, pady=5)
+        self.conversion_frame.grid(row=2, column=0, columnspan=6, sticky="nsew", padx=5, pady=5)
         self.conversion_frame_col_count = 7
-        self.conversion_frame.columnconfigure(self.conversion_frame_col_count)
 
         for i in range(self.conversion_frame_col_count):
             self.conversion_frame.grid_columnconfigure(i, weight=1)
@@ -64,7 +77,10 @@ class MainWindow(tk.Tk):
         self.timezone2_var = tk.StringVar()
         self.timezone2_var.set("-")
         self.timezone2 = ttk.Combobox(self.conversion_frame, textvariable=self.timezone2_var, state="readonly")
-        self.timezone2.grid(row=0, column=2, columnspan=3, sticky="ew", padx=5, pady=5)
+        self.timezone2.grid(row=0, column=2, columnspan=2, sticky="ew", padx=5, pady=5)
+
+        self.switch = tk.Button(self.conversion_frame, image=self.btn_img, command=self.switch)
+        self.switch.grid(row=0, column=4, padx=5, pady=5)
 
         self.result_frame = ttk.Frame(self.conversion_frame, relief="groove")
         self.result_frame.grid(row=1, column=1, columnspan=5, sticky="nsew", padx=5, pady=5)
@@ -72,16 +88,53 @@ class MainWindow(tk.Tk):
         self.result_frame.grid_columnconfigure(0, weight=1)
 
         self.result_text = tk.StringVar()
-        self.result_text.set("00 : 00")
         self.result = tk.Label(self.result_frame, textvariable=self.result_text)
         self.result.grid(sticky="nsew", padx=10, pady=10)
 
+        self._config()
+
+    def _config(self):
+        self.config(menu=self.menu)
+        self.change_time_format()
+
+        self.hour.bind("<FocusOut>", lambda e: self.on_focusout(e) or self.display(e))
+        self.hour.bind("<KeyRelease>", self.display)
+        self.minute.config(validate="key", validatecommand=(self.min_reg, '%P'))
+        self.minute.bind("<FocusOut>", lambda e: self.on_focusout(e) or self.display(e))
+        self.minute.bind("<KeyRelease>", self.display)
+        self.conversion_frame.columnconfigure(self.conversion_frame_col_count)
         self.timezone1.config(postcommand=lambda :self.update_options(self.timezone1, self.timezone2))
         self.timezone2.config(postcommand=lambda :self.update_options(self.timezone2, self.timezone1))
         self.timezone1.bind("<FocusOut>", self.display)
         self.timezone2.bind("<FocusOut>", self.display)
         self.timezone1.bind("<<ComboboxSelected>>", self.display)
         self.timezone2.bind("<<ComboboxSelected>>", self.display)
+
+    def set_day_type(self, widget):
+        if widget:
+            self.am_btn.configure(relief=tk.SUNKEN)
+            self.pm_btn.configure(relief=tk.RAISED)
+        else:
+            self.pm_btn.configure(relief=tk.SUNKEN)
+            self.am_btn.configure(relief=tk.RAISED)
+
+        utils.set_day_type(widget)
+        print(utils.day_type)
+
+    def change_time_format(self):
+        utils.set_time_format(int(self.time_format.get()))
+        if utils.time_format == 24:
+            self.pm_btn.configure(relief=tk.RAISED)
+            self.am_btn.configure(relief=tk.RAISED)
+            self.am_btn.configure(state=tk.DISABLED)
+            self.pm_btn.configure(state=tk.DISABLED)
+            self.input_hr.set("00")
+            self.input_min.set("00")
+        else:
+            self.input_hr.set("12")
+            self.input_min.set("00")
+            self.am_btn.configure(state=tk.NORMAL)
+            self.pm_btn.configure(state=tk.NORMAL)
 
     def update_options(self, w1, w2):
         w1_picked = w1.get()
@@ -112,10 +165,19 @@ class MainWindow(tk.Tk):
 
     @utils.time_filter
     def hour_filter(self, key):
-        if int(key) <= 23:
-            return True
+        if utils.time_format == 24:
+            time =  utils.time_format-1
+            if int(key) <= time:
+                return True
+            else:
+                return False
         else:
-            return False
+            time = utils.time_format
+            if 0 < int(key) <= time:
+                return True
+            else:
+                return False
+            
 
     @utils.time_filter
     def minute_filter(self, key):
@@ -126,21 +188,19 @@ class MainWindow(tk.Tk):
     
     def update_result(self, val1=None, val2=None, day=None):
 
-        if val1 == "":
-            val1 = "00"
-
-        if val2 == "":
-            val2 = "00"
-
-        if val1 == None or val2 == None:
+        if val2 == None:
             result = "Error"
+        
+        elif val1 != "" and val2 != "":
+            result = f"{val1:02} : {val2:02} Day {day:+}"       
         else:
-            result = f"{val1} : {val2} Day {day:+}"
+            result = f"{val1:02} : {val2:02}"
 
         self.result_text.set(result)
-        
 
     def display(self, event):
+        day = 0
+        day_type = None
         value1 = self.hour.get()
         value2 = self.minute.get()
 
@@ -148,24 +208,35 @@ class MainWindow(tk.Tk):
         time2 = self.timezone2_var.get()
 
         try:
-            timezone1 = self.timezones[time1]
-            hour, minute = timezone1(value1, value2, reverse=True)
+            diff1 = self.timezones[time1]
+            hour, minute = utils.convert(value1, value2, diff1, reverse=True)
 
-            timezone2 = self.timezones[time2]
-            value1, value2, day = timezone2(hour, minute)
+            diff2 = self.timezones[time2]
+            value1, value2, day = utils.convert(hour, minute, diff2)
+
         except KeyError as ke:
             print(ke)
         
         except ValueError as ve:
             print(ve)
 
-        print(time1, time2)
-        print(value1, value2)
-
-        if (self.timezone1.get() != "-") and (self.timezone2.get() != "-"):
+        if (self.timezone1.get() != "-"):
             self.update_result(value1, value2, day=day)
+                
         else:
             self.update_result()
+
+    def switch(self):
+        time1 = self.timezone1_var.get()
+        time2 = self.timezone2_var.get()
+
+        self.timezone1_var.set(time2)
+        self.timezone2_var.set(time1)
+
+        self.display(None)
+    
+    def update(self):
+        self.display(None)
 
 if __name__ == "__main__":
     app = MainWindow()
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,73 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import os
 
-now = datetime.now().strftime("%m")
+now = datetime.now().strftime("%m %A")
+today_yr = datetime.now().strftime("%Y")
+
+app_path = os.path.abspath(os.getcwd())
+imgs_path = os.path.join(app_path, "imgs")
+SWITCH_IMG = os.path.join(imgs_path, "switch.png")
+ICON_IMG = os.path.join(imgs_path, "icon.ico")
+
+TIMEZONES_ABBR = [ 
+	("PST", "PDT"), 
+	("MST", "MDT"), 
+	("CST", "CDT"), 
+	("EST", "EDT"), 
+	("AST", "ADT"), 
+	("BRT", None), 
+	("FNT", None), 
+	("AZOT", None), 
+	("GMT", None), 
+	("CET", "CEST"), 
+	("EET", "EEST"), 
+	("MSK", None), 
+	("GST", None), 
+	("PKT", None), 
+	("CAT", None), 
+	("ICT", None), 
+	("HKT", None), 
+	("JST", None), 
+	("AEST", "AEDT"), 
+	("SBT", None), 
+	("NZST", "NZDT") 
+	]
+TIMEZONE = {}
+
+time_format = 24
+day_type = None
+
+def set_day_type(value):
+	global day_type
+	day_type = value
+
+def set_time_format(value):
+	global time_format
+	time_format = value
+
+def get_n_sunday(month, n=0):
+	sundays = []
+	date = datetime(int(today_yr), month, 1)
+	date_cpy = date
+
+	while date_cpy.month == month:
+
+		if date_cpy.weekday() == 6:
+			sundays.append(date_cpy.day)
+
+		date_cpy += timedelta(days=1)
+
+	return date.month, sundays[n]
+
+def is_day_light(start, end):
+    start_month, start_day = start
+    end_month, end_day = end
+    
+    current_date = datetime.now()
+    start_date = datetime(int(today_yr), start_month, start_day)
+    end_date = datetime(int(today_yr), end_month, end_day)
+    
+    return current_date >= start_date and current_date <= end_date
 
 def time_filter(func):
     def wrapper(self, key):
@@ -15,119 +82,55 @@ def time_filter(func):
     
     return wrapper
 
-def pt_converter(hour, minute, reverse=False):
-	diff = -7
+def convert(hour, minute, diff, reverse=False):
+	global time_format, day_type
 	day = 0
+
+	if reverse:
+		if diff > 0:
+			return int(hour) + -abs(diff), minute
+		else:
+			return int(hour) + abs(diff), minute
+
+	hour = int(hour) + diff
+
+	if int(hour) < 0:
+		hour = time_format + int(hour)
+		day -= 1
+
+	if int(hour) >= time_format:
+		hour = hour - time_format
+		day += 1
+
+	return int(hour), minute, day
+
+def get_offset(start:tuple, end:tuple, offset:int):
+	month_start, sun_1 = start 
+	month_end, sun_2 = end
+	offset+=1 if is_day_light(get_n_sunday(month_start, sun_1), get_n_sunday(month_end, sun_2)) else offset
+	return offset
+
+for index, timezone in enumerate(TIMEZONES_ABBR):
+	offset = index - 8
+	start_date, end_date = None, None
+
+	if timezone[1] == None:
+		TIMEZONE[timezone[0]] = offset
+		continue
+
+	if offset <= -4 and is_day_light(get_n_sunday(3, 1), get_n_sunday(11, 0)):
+		TIMEZONE[timezone[1] + " (Daylight Saving)"] = offset + 1
 	
-	if reverse:
-		return int(hour) + abs(diff), minute
-
-	hour = int(hour) + diff
-
-	if int(hour) < 0:
-		hour = 24 + int(hour)
-		day -= 1
+	elif 0 < offset <= 2 and is_day_light(get_n_sunday(3, -1), get_n_sunday(10, -1)):
+		TIMEZONE[timezone[1] + " (Daylight Saving)"] = offset + 1
 	
-	if int(hour) > 24:
-		hour = hour - 24
-		day += 1
-		
-	return int(hour), minute, day
+	elif offset == 10 and is_day_light(get_n_sunday(10, 0), get_n_sunday(4, 0)):
+		TIMEZONE[timezone[1] + " (Daylight Saving)"] = offset + 1
 
-def mt_converter(hour, minute, reverse=False):
-	diff = -6
-	day = 0
+	elif offset == 12 and is_day_light(get_n_sunday(9, -1), get_n_sunday(4, 0)):
+		TIMEZONE[timezone[1] + " (Daylight Saving)"] = offset + 1
+	
+	else:
+		TIMEZONE[timezone[0]] = offset
 
-	if reverse:
-		return int(hour) + abs(diff), minute
-
-	hour = int(hour) + diff
-
-	if int(hour) < 0:
-		hour = 24 + int(hour)
-		day -= 1
-
-	if int(hour) > 24:
-		hour = hour - 24
-		day += 1
-
-	return int(hour), minute, day
-
-def ct_converter(hour, minute, reverse=False):
-	diff = -5
-	day = 0
-
-	if reverse:
-		return int(hour) + abs(diff), minute
-
-	hour = int(hour) + diff
-
-	if int(hour) < 0:
-		hour = 24 + int(hour)
-		day -= 1
-
-	if int(hour) > 24:
-		hour = hour - 24
-		day += 1
-
-	return int(hour), minute, day
-
-def et_converter(hour, minute, reverse=False):
-	day = 0
-	diff = -4
-
-	if reverse:
-		return int(hour) + abs(diff), minute
-
-	hour = int(hour) + diff
-
-	if hour < 0:
-		hour += 24
-		day -= 1
-
-	if hour > 24:
-		hour -= 24
-		day += 1
-
-	return int(hour), minute, day
-
-def utc_converter(hour, minute, reverse=False):
-	diff = 0
-	day = 0
-
-	if reverse:
-		return int(hour) + abs(diff), minute
-
-	hour = int(hour) + diff
-
-	if int(hour) < 0:
-		hour = 24 + int(hour)
-		day -= 1
-
-	if int(hour) > 24:
-		hour = hour - 24
-		day += 1
-
-	return int(hour), minute, day
-
-def jst_converter(hour, minute, reverse=False):
-	diff = 9
-	day = 0
-
-	if reverse:
-		return int(hour) + -abs(diff), minute
-
-	hour = int(hour) + diff
-
-	if int(hour) < 0:
-		hour = 24 + int(hour)
-		day -= 1
-
-	if int(hour) > 24:
-		hour = hour - 24
-		day += 1
-
-	return int(hour), minute, day
-
-if __name__ == "__main__":
-	print(et_converter(23, 3))
+print(TIMEZONE)
